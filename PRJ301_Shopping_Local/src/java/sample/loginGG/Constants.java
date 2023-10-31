9cc16af4b91611bcb38617959103c696712233f1
dhp131 <daophuong131.work@gmail.com>
2023-10-31 08:53:25 +0700

    done

--- PRJ301_Shopping_Local/src/java/sample/loginGG/Constants.java
+++ PRJ301_Shopping_Local/src/java/sample/loginGG/Constants.java
@@ -15,7 +15,7 @@ public class Constants {
 
     public static String GOOGLE_CLIENT_SECRET = "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";
 
-    public static String GOOGLE_REDIRECT_URI = "http://localhost:8084/PRJ301_T2S2_Assignment/LoginGoogle";
+    public static String GOOGLE_REDIRECT_URI = "http://localhost:8084/PRJ301_Shopping_Local/LoginGoogle";
 
     public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
 
